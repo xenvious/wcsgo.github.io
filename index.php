61cd45bfcb94db67747e2bfd880faad827b09a66
xenvious <vindictful@gmail.com>
2018-08-27 21:20:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="WCS:GO Server">
-    <meta name="author" content="Leg[a]cy (STEAM_0:1:15645964)">
+    <meta name="description" content="Preston High School | Online Textbooks">
+    <meta name="author" content="Leg[a]cy">
 
-    <title>WCS:GO</title>
+    <title>Preston High School | Online Textbooks</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
